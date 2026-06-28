d4ed11f891ac6b882402eb266d053b80c938da60
krish kumar <Krish-CyberSec@users.noreply.github.com>
2026-06-28 14:34:23 +0530

    restructure

--- src/pages/main/Error.jsx
+++ src/pages/main/Error.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 import styled from "styled-components";
-import logo from "../assets/logo.png";
+import logo from "../../assets/logo.png";
 
 const StyledLink = styled(NavLink)`
   background: var(--university-blue-dark);
