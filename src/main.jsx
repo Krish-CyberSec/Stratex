367304b82fb6fb339a44111e442604a11333f9d2
krish kumar <Krish-CyberSec@users.noreply.github.com>
2026-07-12 14:45:51 +0530

    coustomised scrollbar

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,8 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import SimpleBar from "simplebar-react";
+import "simplebar-react/dist/simplebar.min.css";
 import "./index.css";
 import App from "./App.jsx";
 import { AuthProvider } from "./context/AuthContext.jsx";
@@ -9,7 +11,9 @@ createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
       <AuthProvider>
-        <App />
+        <SimpleBar style={{ height: "100vh" }}>
+          <App />
+        </SimpleBar>
       </AuthProvider>
     </BrowserRouter>
   </StrictMode>,
